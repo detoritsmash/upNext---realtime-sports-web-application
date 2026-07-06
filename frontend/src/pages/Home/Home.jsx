7d33974a2348b5bb6adfa80ff02dd896908ff161
Tejas Nayak <tejastnayak@gmail.com>
2026-07-06 23:08:20 +0530

    feat(home): add homepage sections and navigation components

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,11 @@
 import Navbar from "../../components/layout/Navbar";
 import BottomNavigation from "../../components/layout/BottomNavigation";
 import PageLayout from "../../components/layout/PageLayout";
+
+import SportsTabs from "../../components/ui/SportsTabs";
+import LeagueCarousel from "../../components/league/LeagueCarousel";
+import DateTabs from "../../components/ui/DateTabs";
+
 import LiveMatchesSection from "../../components/match/LiveMatchesSection";
 
 const Home = () => {
@@ -9,7 +14,13 @@ const Home = () => {
       <Navbar />
 
       <main className="pb-24">
+        <SportsTabs />
+
+        <LeagueCarousel />
+
         <LiveMatchesSection />
+
+        <DateTabs />
       </main>
 
       <BottomNavigation />
